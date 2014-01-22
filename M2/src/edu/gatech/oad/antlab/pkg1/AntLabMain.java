4a4832d82342df603c38387f735a4da2e4c67b92
Kerry <Kerry.Brent.Smith@gmail.com>
2014-01-21 19:11:51 -0500

    Replaced "P5" with "Kerry Smith." Replaced "gburdell5" with "ksmith89."

--- M2/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Kerry Smith");
-                  toPrint += p5.toString("KSmith89");
+                  toPrint += p5.toString("ksmith89");
 		  
         System.out.println(toPrint);
         
